364e07c5465b795c9cb5486ee9138a80da258ee3
Bruno Silva <s.bruno.12@hotmail.com>
2023-10-24 06:32:50 -0300

    first commit

---
+++ src/exercicio/Wrapper.java
@@ -0,0 +1,14 @@
+package exercicio;
+
+public class Wrapper {
+    public static void main(String[] args) {
+
+        int numeroPrimitivo = 42;
+
+
+        Integer numeroWrapper = numeroPrimitivo;
+
+        System.out.println("Número como primitiva: " + numeroPrimitivo);
+        System.out.println("Número como wrapper: " + numeroWrapper);
+    }
+}
